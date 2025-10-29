7870c178a3c7c9f0a9cda7a3c336ad90779692fc
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-10-29 12:41:43 +0100

    Bump version to 3.4.0 and update index.js with Zod validation

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// @ts-check
+
+const { default: z } = require("zod")
+
 console.log('søppelplassen')
 
 console.log('Her er det søppel')
 console.log('Her er det mer søppel nå')
 console.log('Her er det enda mer søppel nå')
 console.log('Her er det enda mer søppel en det var før nå')
+
+const TUT = z.object({
+  name: z.string(),
+  age: z.number(),
+})
+
+
+
+const tut = TUT.parse({a: 'Ola', age: 'dfjkd'})
+
+console.log('Søppel er gøy')
